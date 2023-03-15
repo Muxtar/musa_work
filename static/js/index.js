bb6abf6643d433a0f4994914da8aba0ab2b7bddd
Muxtar <muxtar.bayramov92@gmail.com>
2023-03-15 11:53:07 +0400

    version2

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,17 @@
-function backgroundImage(){
-    let header = document.getElementsByTagName('header')[0];
-    let image = header.getElementsByTagName('img')[0];
-    image.src = '../static/images/carusel.jpg';
-}
+// function backgroundImage(){
+//     let header = document.getElementsByTagName('header')[0];
+//     let image = header.getElementsByTagName('img')[0];
+//     image.src = '../static/images/carusel.jpg';
+// }
 
-backgroundImage()
+// backgroundImage()
+
+let divRight = document.getElementsByClassName('right-border')[0];
+
+let borderRigth = setTimeout(()=>{
+    divRight.style.height = '100%';
+}, 5000)
+
+let changeImage = setTimeout(()=>{
+    document.getElementById('image').src = '../static/images/carusel.jpg';
+}, 10000)
